1c87b96f37ee12e8600a6f913e6cc00927c6f6e4
Spectrumleeee <spectrumleeee@gmail.com>
2014-08-04 10:03:00 +0800

    Code formatted in NioClient Project!

--- workplace/NioClient/src/main/java/com/tplink/cloud/lgp/NioClient/ManagerClient.java
+++ workplace/NioClient/src/main/java/com/tplink/cloud/lgp/NioClient/ManagerClient.java
@@ -1,10 +1,16 @@
+/**
+ * NioClient entity
+ * Copyright (c) 2014, TP-Link Co.,Ltd.
+ * Author: liguangpu <liguangpu@tp-link.net>
+ * Updated: Aug 4, 2014
+ */
+
 package com.tplink.cloud.lgp.NioClient;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
-
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -12,40 +18,41 @@ public class ManagerClient {
 
 	private static Logger log;
 	private InetSocketAddress inetSocketAddress;
-	
-	public ManagerClient(String hostname, int port){
+
+	public ManagerClient(String hostname, int port) {
 		PropertyConfigurator.configure("log4j.properties");
 		log = Logger.getLogger(NioClient.class.getName());
 		inetSocketAddress = new InetSocketAddress(hostname, port);
 	}
-	
-	public void send(String requestDataType, String requestData){
+
+	public void send(String requestDataType, String requestData) {
 		try {
 			SocketChannel socketChannel = SocketChannel.open(inetSocketAddress);
 			socketChannel.configureBlocking(false);
 			ByteBuffer byteBuffer = ByteBuffer.allocate(512);
 
 			String temp = requestDataType + requestData;
-			
+
 			socketChannel.write(ByteBuffer.wrap(temp.getBytes()));
-			
+
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			while (true) {
-				
+
 				byteBuffer.clear();
 				int readBytes = socketChannel.read(byteBuffer);
-				
+
 				if (readBytes > 0) {
 					byteBuffer.flip();
 					log.info("ManagerClient: readBytes = " + readBytes);
-					log.info("ManagerClient: data = " + new String(byteBuffer.array(), 0, readBytes));
-				}else{
+					log.info("ManagerClient: data = "
+							+ new String(byteBuffer.array(), 0, readBytes));
+				} else {
 					socketChannel.close();
 					break;
 				}
@@ -55,18 +62,23 @@ public class ManagerClient {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		String hostname = "localhost";
-//		String requestDataType = "11112AA";
-		String requestDataType = "11112DD";
-//		String requestData = "Actions speak louder than words! AA";
-		String requestData = "No zuo No die!";
-		
 		int port = 8888;
-		new NioClient(hostname, port).send(requestDataType, requestData);
+
+		// the header consist of 7 bits, first 4 bits stand for the client id,
+		// last 2 bits means subject id, the 5th bit means this is a connection
+		// from the manager client.
+
+		// String requestDataType = "11112AA";
+		// String requestData = "Actions speak louder than words! AA";
+		String header = "11112DD";
+		String requestData = "No zuo No die!";
+
+		new NioClient(hostname, port).send(header, requestData);
 	}
 
 }
--- workplace/NioClient/src/main/java/com/tplink/cloud/lgp/NioClient/NioClient.java
+++ workplace/NioClient/src/main/java/com/tplink/cloud/lgp/NioClient/NioClient.java
@@ -1,68 +1,67 @@
+/**
+ * NioClient entity
+ * Copyright (c) 2014, TP-Link Co.,Ltd.
+ * Author: liguangpu <liguangpu@tp-link.net>
+ * Updated: Aug 4, 2014
+ */
+
 package com.tplink.cloud.lgp.NioClient;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
-
-
-
-//import java.util.logging.Logger;
 import org.apache.log4j.*;
 
+/**
+ * The class creates a Client based on NIO TCP
+ */
 public class NioClient {
 
 	private static Logger log;
 	private InetSocketAddress inetSocketAddress;
-	
+
 	public NioClient(String hostname, int port) {
 		PropertyConfigurator.configure("log4j.properties");
 		log = Logger.getLogger(NioClient.class.getName());
 		inetSocketAddress = new InetSocketAddress(hostname, port);
 	}
-	
+
 	/**
-	 * 发送请求数据
+	 * send the request data with different header type
+	 * 
 	 * @param requestData
-	 * @throws InterruptedException 
+	 * @throws InterruptedException
 	 */
-	public void send(String requestDataType, String requestData){
+	public void send(String requestDataType, String requestData) {
 		try {
 			SocketChannel socketChannel = SocketChannel.open(inetSocketAddress);
 			socketChannel.configureBlocking(false);
 			ByteBuffer byteBuffer = ByteBuffer.allocate(512);
-			
-//			socketChannel.write(ByteBuffer.wrap(requestDataType.getBytes()));
-//			byteBuffer.clear();
-			String temp = requestDataType + requestData;
-			
-			socketChannel.write(ByteBuffer.wrap(temp.getBytes()));
-			
+
+			String message = requestDataType + requestData;
+			// write the message to the socketChannel
+			socketChannel.write(ByteBuffer.wrap(message.getBytes()));
+
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			while (true) {
-				
 				byteBuffer.clear();
 				int readBytes = socketChannel.read(byteBuffer);
-				
+
 				if (readBytes > 0) {
 					byteBuffer.flip();
 					log.info("Client: readBytes = " + readBytes);
-					log.info("Client: data = " + new String(byteBuffer.array(), 0, readBytes));
-				}else{
+					log.info("Client: data = "
+							+ new String(byteBuffer.array(), 0, readBytes));
+				} else {
 					socketChannel.close();
 					break;
-//					try {
-//						Thread.sleep(1000);
-//					} catch (InterruptedException e) {
-//						// TODO Auto-generated catch block
-//						e.printStackTrace();
-//					}
 				}
 			}
 
@@ -70,33 +69,27 @@ public class NioClient {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		
 		String hostname = "localhost";
-		
-		/**
-		 * Here define two message types
-		 * when server get '1111' , it will return yes and repeat the message from client
-		 * if server get '0000' , it will return no and repeat the message.
-		 * others(but must be for bytes ) it'll just repeat the message.
-		 * 
-		 * sometimes we just need 8 bits can make it 
-		 */
-		
-//		String requestDataType = "1010";
-		String create1 = "000011010";
-		String read1 = "00000XXXX";
-		String create2 = "001110011";
-		String read2 = "00110XXXX";
-//		String requestDataType = "1111";
-		String requestData = "Actions speak louder than words!";
-		
 		int port = 8888;
-//		new NioClient(hostname, port).send(create1, requestData);
-//		new NioClient(hostname, port).send(create2, requestData);
-		
-		new NioClient(hostname, port).send(read1, requestData);
-		new NioClient(hostname, port).send(read2, requestData);
+		/**
+		 * The header is consist of 9 bits, the former 4 bits stand for the
+		 * client id, the laster 4 bits stand for the subject this client will
+		 * subscribe, the middle bit '1' means establish a connection and '0'
+		 * means get message from server
+		 */
+		String header1 = "000011010";
+		String header11 = "00000XXXX";
+		String header2 = "001110011";
+		String header22 = "00110XXXX";
+		String requestData = "Words useless";
+
+		// new NioClient(hostname, port).send(header1, requestData);
+		// new NioClient(hostname, port).send(header2, requestData);
+
+		new NioClient(hostname, port).send(header11, requestData);
+		new NioClient(hostname, port).send(header22, requestData);
 	}
 }
